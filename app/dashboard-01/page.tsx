09db743e29d6c112f3340f83846b2c619e80aa71
QuentinCody <33259999+QuentinCody@users.noreply.github.com>
2024-06-15 09:28:33 -0400

    feat: Add @radix-ui/react-dialog and @radix-ui/react-dropdown-menu dependencies

---
+++ app/dashboard-01/page.tsx
@@ -0,0 +1,11 @@
+// pages/index.tsx
+"use client"
+
+import React from 'react';
+import DashboardComponent from "@/components/dashboard-01";
+
+const Dashboard: React.FC = () => {
+  return <DashboardComponent />;
+}
+
+export default Dashboard;
